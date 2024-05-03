d4ad304401c74bec32290485fc554f570794457e
Felipe Joglar <felipejoglar@gmail.com>
2024-05-03 23:31:44 +0200

    Remove template files

--- klimate-core/src/androidMain/kotlin/dev/felipejoglar/klimate/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package dev.felipejoglar.klimate
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- klimate-core/src/commonMain/kotlin/dev/felipejoglar/klimate/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.felipejoglar.klimate
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
